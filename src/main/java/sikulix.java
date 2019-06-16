4572bf232ed3b874bb6a667355c443a68e322bb1
Guilherme Machado Rogatto <gui.rogatto@gmail.com>
2019-06-16 18:18:33 -0300

    difinição de arquitetura do projeto

--- src/main/java/sikulix.java
+++ src/main/java/sikulix.java
@@ -1,3 +1,15 @@
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
+
 public class sikulix {
 
+
+    public static void main(String[] args) throws FindFailed {
+
+
+        Screen sc = new Screen();
+        Pattern image = new Pattern();
+        sc.wait(image, 19);
+    }
 }
