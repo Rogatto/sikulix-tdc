e509ca86bea5657684992fd328552ca34fe65143
Guilherme Machado Rogatto <gui.rogatto@gmail.com>
2019-07-09 19:51:50 -0300

    adicionando testes por contexto

--- src/test/java/tests/TestGoogle.java
+++ src/test/java/tests/TestGoogle.java
@@ -12,6 +12,9 @@ import org.sikuli.script.FindFailed;
 import org.sikuli.script.Pattern;
 import org.sikuli.script.Screen;
 import pageobject.Login;
+
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.assertTrue;
 
@@ -20,12 +23,21 @@ public class TestGoogle {
     private WebDriver driver;
     private String baseUrl = "https://www.google.com.br/";
     private Screen screen;
+    private String testContext;
 
     @Before
-    public void setUp(){
+    public void setUp() throws IOException {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        testContext = System.getProperty("testContext");
+
+        if(testContext == null) {
+            testContext = "funcional";
+        }
+
+        System.out.println("Contexto do teste: " + testContext);
     }
 
     @Test
@@ -67,6 +79,43 @@ public class TestGoogle {
         } else {
             System.out.println("não existe");
             assertTrue(false);
+        }
+    }
+
+    @Test
+    public void testFunctionalAndVisual() throws InterruptedException, FindFailed {
+
+        driver.get(baseUrl);
+
+        By imagemGoogle = By.id("hplogo");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(imagemGoogle));
+
+        if(testContext.contains("functional")){
+
+            if(driver.findElements(imagemGoogle).isEmpty()){
+                assertTrue(false);
+            } else {
+                assertTrue(true);
+            }
+
+        } else if (testContext.contains("visual")) {
+
+            screen = new Screen();
+            Pattern image = new Pattern("/Users/guilhermerogatto/Documents/git/sikulix-tdc/src/test/resources/imgs/imagem_google.png");
+            System.out.println(image.getFilename());
+
+            screen.wait(image, 10);
+
+            String img = screen.capture().save("/Users/guilhermerogatto/Documents/git/sikulix-tdc/src/test/resources/visualresults", "imagem_google");
+
+            if(screen.has(image)){
+                System.out.println("existe");
+                assertTrue(true);
+            } else {
+                System.out.println("não existe");
+                assertTrue(false);
+            }
         }
 
     }
